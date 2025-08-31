88867d7b54d6ad3393c7eb224c83357d2cbe300b
RISHAV KUMAR <rishavkr43125@gmail.com>
2025-08-31 18:52:02 +0530

    Update script.js

--- Frontend_vc/script.js
+++ Frontend_vc/script.js
@@ -1,5 +1,5 @@
 //  const baseUrl = 'http://127.0.0.1:5000';
-const baseUrl = 'https://finstra-production.up.railway.app/';
+const baseUrl = 'https://finstra.onrender.com';
 
 const startBtn = document.getElementById('start-btn');
 const resultDiv = document.getElementById('result');
